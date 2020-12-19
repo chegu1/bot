a5dd2fe8f408abce938720c9c77bdc3ad9cf1673
chegu manikanta kumar <makeurproject3@gmail.com>
2020-12-20 01:59:36 +0530

    second

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,6 +1,6 @@
 module.exports = function (app, chalk) {
     app.get('/webhook', function (req, res) {
-        if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+        if (req.query['hub.verify_token'] === 'EAAC7rfotrGoBAJ5XnbuPrwiqyZBLhNyRtoynSV7T4uxOya8ZBowpY8kvPdGuxhZBkUcGtlVgzPgYxuzk5pEtZAQwNxwrcrfZACs5Gks5ZBY5AYSXzP4mWFn0YZAAog3RkNovP2sqdg2ZANdlYi9v1BA0lSx8e50J9aCq0eZA3Y6aa3ATZCthZC0uFourDZC6pLC9GzYZD') {
             console.log('webhook verified');
             res.status(200).send(req.query['hub.challenge']);
         } else {
